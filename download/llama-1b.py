b9c3f94e0e019bab18c578559c61ccda6e060a87
Acaller <12556752+acaller@user.noreply.gitee.com>
2025-04-03 11:03:45 +0800

    修改了保存路径

--- download/llama-1b.py
+++ download/llama-1b.py
@@ -1,6 +1,3 @@
 import os
-
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-
-os.system('huggingface-cli download --resume-download meta-llama/Llama-3.2-1B --local-dir D:\\desktop\\merage\\models\\llama-1b')
+os.system('huggingface-cli download --resume-download meta-llama/Llama-3.2-1B --local-dir /content/EmbeddingMerage/models/llama-1b')
 # meta-llama/Llama-3.2-3B
--- download/llama-3b.py
+++ download/llama-3b.py
@@ -1,6 +1,4 @@
 import os
 
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-
-os.system('huggingface-cli download --resume-download meta-llama/Llama-3.2-3B --local-dir D:\\desktop\\merage\\models\\llama-3b')
+os.system('huggingface-cli download --resume-download meta-llama/Llama-3.2-3B --local-dir /content/EmbeddingMerage/models/llama-3b')
 # meta-llama/Llama-3.2-3B
